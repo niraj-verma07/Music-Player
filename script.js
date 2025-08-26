637cd375676e529fe12a2c9962753ed9e9cdec8b
niraj-verma07 <vermaniraj9607@gmail.com>
2025-08-26 19:07:28 +0530

    Structure

---
+++ script.js
@@ -0,0 +1,17 @@
+var arr = [
+    { songName: "Bezubaan(Phir se)", url: "./Songs/Bezubaan.m4a", img: "./Images/Bezubaan.jpg" },
+    
+    { songName: "Chunar", url: "./Songs/chunar.m4a", img: "./Images/Bezubaan.jpg" },
+
+    { songName: "Hamari Adhuri Kahani", url: "./Songs/Hamari.m4a", img: "./Images/Hamari.jpg" },
+
+    { songName: "Lambiyan Jhudaiyan", url: "./Songs/Lambiya.m4a", img: "./Images/Shersah.jpg" },
+
+    { songName: "Millionaire", url: "./Songs/Millionaire.m4a", img: "./Images/Millionaire.jpg" },
+
+    { songName: "Ranjha", url: "./Songs/Ranjha.m4a", img: "./Images/Shersah.jpg" },
+   
+]
+
+
+
